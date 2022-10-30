fc972f905e4f3b9281a00c20bebfa80f48cda025
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2022-10-30 15:59:17 +0100

    Upload Files With js Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
-// menu
+//! ================================
+//* ============ menu ==============
+//! ================================
 let menuBurger = document.querySelector(".menu-burger");
 let closeMenu = document.querySelector(".close-menu");
 let navMenu = document.querySelector(".navigation");
-
 const openNavMenu = ()=>{
     navMenu.style.transform = 'translateX(0)';
 };
 const closeNavMenu = ()=>{
     navMenu.style.transform = 'translateX(100%)';
 };
-
 menuBurger.addEventListener('click', openNavMenu);
 closeMenu.addEventListener('click', closeNavMenu);
 
-// food filters
+//! ================================
+//* ========= Filters ==============
+//! ================================
 let filters = document.querySelectorAll(".filter");
 let meals = document.querySelectorAll(".meal");
 
@@ -39,12 +41,84 @@ function manageMeals() {
     })
 }
 
-// Products Cart
+//! ================================
+//* ============ Cart ==============
+//! ================================
 let cartIcon = document.querySelector(".cart");
 let productsCart = document.querySelector(".meals-cart");
-
+// reveal cart
 const revealCart = () => {
     productsCart.classList.toggle("cart-active");
 }
-
 cartIcon.addEventListener("click", revealCart);
+
+//! ================================
+//* ==== Add Products To Cart ======
+//! ================================
+let addToCartBtn = document.getElementsByClassName("add-to-cart");
+for(let i = 0; i < addToCartBtn.length; i++) {
+    let btn = addToCartBtn[i];
+    btn.addEventListener("click", selectClickedItem);
+}
+
+function selectClickedItem(e){
+    let button = e.target;
+    let itemImage = button.parentElement.parentElement.parentElement.firstElementChild.getAttribute("src");
+    let itemTitle = button.parentElement.parentElement.parentElement.firstElementChild.nextElementSibling.firstElementChild.innerText;
+    let itemPrice = button.parentElement.parentElement.parentElement.firstElementChild.nextElementSibling.lastElementChild.innerText;
+    console.log(itemImage, itemTitle, itemPrice)
+    addItemToCart(itemImage, itemTitle, itemPrice);
+}
+
+let mealsCart = document.querySelector(".cart-products");
+let total = 0;
+let totalElement = document.querySelector(".total-price");
+function addItemToCart(img, title, price) {
+    let quantityCountSpan = document.querySelector(".quantity-span");
+    let itemCard = `
+    <!-- product -->
+    <div class="product">
+        <img src="${img}" alt="" width="100%">
+        <h3 class="product-title">${title}</h3>
+        <div class="price-quantity">
+            <p>${price}</p>
+            <p>Quantity: </p>
+        </div>
+    </div>`;
+    mealsCart.innerHTML += itemCard;
+    // total products in cart
+    quantityCountSpan.innerText++;
+    // update total
+    let productPrice = price.replace("$", "");
+    total += parseInt(productPrice);
+    totalElement.innerText = `Total : ${total}`;
+
+}
+
+// PURCHASE CART
+let purchaseBtn = document.querySelector(".purchase-cart");
+let purchaseModal = document.querySelector(".purchase-success");
+purchaseBtn.addEventListener("click", purchaseCart);
+function purchaseCart() {
+    mealsCart.innerText = "";
+    document.querySelector(".quantity-span").innerText = 0;
+    count = 1;
+    total = 0;
+    totalElement.innerText = "Total : ";
+    purchaseModal.style.display = 'block';
+    setInterval(()=>{
+        purchaseModal.style.display = 'none';
+    }, 2000);
+}
+
+//! ================================
+//* ==== Clear Cart Function =======
+//! ================================
+let clearBtn = document.querySelector(".clear-cart").addEventListener("click", clearCart);
+function clearCart() {
+    mealsCart.innerText = "";
+    document.querySelector(".quantity-span").innerText = 0;
+    count = 1;
+    total = 0;
+    totalElement.innerText = "Total : ";
+}
